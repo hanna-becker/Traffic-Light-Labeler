1d520772c6192be2a5dba95f0920d3f086c429b3
Hanna Becker <hanna.e.becker@gmail.com>
2019-05-22 00:06:47 +0200

    refactor: extract labeler module from main module

---
+++ traffic-light-labeler/labeler.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from user_input import get_user_input
+
+IMG_DIR = 'images'
+LABELED_DATA_FILE = "labeled_data.csv"
+
+
+def _print_finish_msg():
+    print("\n******************************")
+    print("Finished labeling all images!")
+    print("******************************\n")
+
+
+def run():
+    labeled_data_file = open(LABELED_DATA_FILE, "a")
+    img_paths = Path(IMG_DIR).glob('**/*.jpg')
+    tl_state = None
+    plt.ion()
+    plot = plt.imshow(np.zeros((600, 800)))
+    for img_path in img_paths:
+        path = str(img_path)
+
+        # check if image has already been labeled
+        if path in open(LABELED_DATA_FILE).read():
+            continue
+
+        image = cv2.imread(path)
+        plot.set_data(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.pause(0.05)
+
+        tl_state = get_user_input()
+        if tl_state == "e":
+            break
+        labeled_data_file.write("%s, %s\n" % (path, tl_state))
+
+    labeled_data_file.close()
+
+    if tl_state != "e":
+        _print_finish_msg()
