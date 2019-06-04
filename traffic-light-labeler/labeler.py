b8081573b42fecd577d90b3a37a54cd2971fc3b2
Hanna Becker <hanna.e.becker@gmail.com>
2019-06-04 22:02:53 +0200

    feat: make image format configurable

--- traffic-light-labeler/labeler.py
+++ traffic-light-labeler/labeler.py
@@ -9,6 +9,7 @@ IMG_DIR = 'images'
 LABELED_DATA_FILE = "labeled_data.csv"
 IMAGE_WIDTH = 600
 IMAGE_HEIGHT = 1000
+IMG_FORMAT = 'png'
 
 
 def _print_finish_msg():
@@ -19,7 +20,7 @@ def _print_finish_msg():
 
 def run():
     labeled_data_file = open(LABELED_DATA_FILE, "a")
-    img_paths = Path(IMG_DIR).glob('**/*.png')
+    img_paths = Path(IMG_DIR).glob('**/*.' + IMG_FORMAT)
     tl_state = None
     plt.ion()
     plot = plt.imshow(np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH)))
